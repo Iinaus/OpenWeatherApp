f69834daccc2d48e414044bc5786e41efabc81f9
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-11-11 15:04:30 +0200

    Improve data printing and add error handling

--- Program.cs
+++ Program.cs
@@ -5,15 +5,42 @@ class Program
 {
     static async Task Main(string[] args)
     {
+        try {
+            var weatherNurmijarvi = await FetchData("Nurmijärvi");
+            var weatherRovaniemi = await FetchData("Rovaniemi");
+
+            printWeatherInfo(weatherNurmijarvi);
+            Console.WriteLine("-----------------------");
+            printWeatherInfo(weatherRovaniemi);   
+                                
+        } catch (System.Exception e) {
+            Console.WriteLine($"Virheen tyyppi: {e.GetType()}");
+            Console.WriteLine($"Virheen viesti: {e.Message}");
+        } 
+    }
+
+    static async Task<Weather> FetchData(string city)
+    {
         HttpClient client = new HttpClient();
         DotEnv.Load();
         string apiKey = Environment.GetEnvironmentVariable("API_KEY");
-        string city = "Helsinki";
         string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric"; 
         var data = client.GetStreamAsync(url);
         var weather = JsonSerializer.Deserialize<Weather>(await data); 
-        Console.WriteLine(weather.name);
-        Console.WriteLine(weather.main.temp);
+        return weather;
     }
 
+    static void printWeatherInfo(Weather weather)
+    {
+        if (weather != null) {               
+            string date = weather.convertDate();
+
+            Console.WriteLine($"Tänään on {date} ja paikassa {weather.name} on seuraavanlainen sää:");
+            Console.WriteLine($"Lämpötila {weather.main.temp} °C, tuntuu kuin {weather.main.feels_like} °C.");
+            Console.WriteLine($"Päivän ylin lämpötila on {weather.main.temp_max} °C ja alin {weather.main.temp_min} °C.");
+            Console.WriteLine($"Ilmankosteus {weather.main.humidity} %.");
+        } else {
+            Console.WriteLine($"Säätilatietoja ei saatavilla.");
+        }
+    }
 }
